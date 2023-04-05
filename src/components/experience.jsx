5d86dc50187ee3821b274e96c5372e6efcc880a7
Hania <hania_nina@live.fr>
2023-04-05 23:03:01 +0200

    Footer and Experience

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -3,13 +3,21 @@ import './HomepageC.css';
 
 const  Exprience=()=> {
   return (
-    <div className='exprience'>
-        <div>
-            <h1>Live the experience with <span className='cl3'>AIESEC</span></h1>
-            <p>Found a project of your liking?</p>
-            <p> hurry up and apply and get matched!</p>
-        </div>
-     
+
+    <div className='bg'>
+      <div className=' c exprience'>
+          <div className='col-5 text'>
+              <h1>Live the experience with <span className='cl3'>AIESEC</span></h1>
+              <p>Found a project of your liking?</p>
+              <p>hurry up and apply and get matched!</p>
+              <button className='btn'>Apply now</button>
+          </div>
+          <div className='col-5 exp-img'>
+          <img className='front' src={require('../img/img5.png')} alt='exprience'/>
+          <img src={require('../img/img4.png')} alt='exprience'/>
+          </div>
+      
+      </div>
     </div>
   )
 }
